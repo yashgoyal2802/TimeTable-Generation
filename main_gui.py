44091c6bcb83fd53d8a251a57b2dcdaeb45a5189
VaradhK <40127598+varadhk@users.noreply.github.com>
2020-03-18 17:28:28 +0530

    New Input Breakdown

--- main_gui.py
+++ main_gui.py
@@ -5,15 +5,43 @@ window.title("Timetable Generation OS Project")
 
 
 def set_values():
-    list_1 = [label3_1.get(), label3_2.get(), label3_3.get(), label3_4.get()]
-    list_2 = [label4_1.get(), label4_2.get(), label4_3.get(), label4_4.get()]
-    list_3 = [label5_1.get(), label5_2.get(), label5_3.get(), label5_4.get()]
-    list_4 = [label6_1.get(), label6_2.get(), label6_3.get(), label6_4.get()]
+    list_1 = [label3_1.get(), label3_2.get(), label3_3.get(), label3_4.get()]       #Batch 1
+    list_2 = [label4_1.get(), label4_2.get(), label4_3.get(), label4_4.get()]       #Batch 2
+    list_3 = [label5_1.get(), label5_2.get(), label5_3.get(), label5_4.get()]       #Batch 3
+    list_4 = [label6_1.get(), label6_2.get(), label6_3.get(), label6_4.get()]       #Batch 4
+    flist = [list_1, list_2, list_3, list_4]
     print(list_1)
     print(list_2)
     print(list_3)
     print(list_4)
+    print(flist)
 
+    fac_list_1 = []                 # Number of lectures by each batch
+    fac_list_2 = []
+    fac_list_3 = []
+    fac_list_4 = []
+
+    for faculty_no in range(0, 4):
+        x = int(flist[faculty_no][0])
+        for hour_cnt in range(0, x):
+            fac_list_1.append(faculty_no)
+
+        x1 = int(flist[faculty_no][1])
+        for hour_cnt in range(0, x1):
+            fac_list_2.append(faculty_no)
+
+        x2 = int(flist[faculty_no][2])
+        for hour_cnt in range(0, x2):
+            fac_list_3.append(faculty_no)
+
+        x3 = int(flist[faculty_no][3])
+        for hour_cnt in range(0, x3):
+            fac_list_4.append(faculty_no)
+
+    print(fac_list_1)
+    print(fac_list_2)
+    print(fac_list_3)
+    print(fac_list_4)
 
 
 text1 = Label(window, text="Enter the faculty hours required for each branch")
